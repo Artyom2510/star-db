e90388a1e79453a2da37295af0667c4ed87a3284
Artyom <taranenko@grechka.digital>
2019-12-04 18:43:59 +0300

    lessons 74

--- src/components/ItemList/itemList.jsx
+++ src/components/ItemList/itemList.jsx
@@ -11,9 +11,9 @@ export default class ItemList extends Component {
 
 	componentDidMount() {
 
-		const { getDate } = this.props;
+		const { getData } = this.props;
 
-		getDate()
+		getData()
 			.then((itemList) => {
 				this.setState({
 					itemList
@@ -22,13 +22,17 @@ export default class ItemList extends Component {
 	}
 
 	renderItems(arr) {
-		return arr.map(({id, name}) => {
+		return arr.map((item) => {
+
+			const { id } = item;
+			const label = this.props.renderItem(item);
+
 			return (
 			<li className="list-group-item"
 				key={id}
 				onClick={() => this.props.onItemSelected(id)}
 			>
-				{name}
+				{label}
 			</li>
 			)
 		});
